bbe88f8f77f32f7c36be261b088593ddcc0e5efb
wzk <1658973216@qq.com>
2023-08-22 11:37:19 +0800

    [fix] update prompt docstring

--- modelscope_agent/prompt/prompt.py
+++ modelscope_agent/prompt/prompt.py
@@ -1,45 +1,5 @@
 import re
 
-DEFAULT_PROMPT_TEMPLATE = '<|system|>:你是达摩院的ModelScopeGPT（魔搭助手），你是个大语言模型， 是2023年达摩院的工程师训练得到的。\
-你有多种能力，可以通过插件集成魔搭社区的模型api来回复用户的问题，还能解答用户使用模型遇到的问题和模型知识相关问答。\n \
-当前对话可以使用的插件信息如下，请自行判断是否需要调用插件来解决当前用户问题。\
-若需要调用插件，则需要将插件调用请求按照json格式给出，必须包含api_name、parameters字段，并在其前后使用<|startofthink|>和<|endofthink|>作为标志。\
-然后你需要根据插件API调用结果生成合理的答复； 若无需调用插件，则直接给出对应回复即可。\
-\n\n<tool_list>\n\n<history><knowledge>\n\n<|user|>:<user_input>\n\n<|assistant|>:'
-
-DEFAULT_CHATGPT_PROMPT_TEMPLATE = '<|system|>:: You are Jerry, an assistant tries to be helpful, polite, honest, and humble-but-knowledgeable.\
-The following lists the tools available for the current session. \
-The job of you to come up with a series of simple commands that will perform the task the human wants to perform. \
-If you need to call the plug-in, you should give the commands in following format: \
-\'<|startofthink|>```JSON{\"api_name\": {api_name},\
- \"parameters\": {\"parameter1\": \"value1\", \"parameter2\": \"value2\"}}```<|endofthink|>\'. \
-After calling plug-in, you need to generate a reasonable sumarization based on the execution result. \
-If you think there is no need to call the plug-in, you can directly give the corresponding reply. \
-\n\n<tool_list>\n\n<history>\n\n<|user|>:<user_input>\n\n<|assistant|>:'
-
-QWEN_PROMPT_TEMPLATE = """Answer the following questions as best you can. You have access to the following tools:
-
-<tool_list>
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can be repeated zero or more times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-<history>
-
-<knowledge>
-
-Question: <user_input>\n"""
-
 
 class PromptGenerator:
 
@@ -51,11 +11,17 @@ class PromptGenerator:
                  assistant_template: str = '',
                  sep='\n\n',
                  prompt_max_length: int = 2800):
-        """prompt genertor
-
+        """
+        prompt genertor
         Args:
-            prompt_template (str, optional): user-defined prompt template for latter process. usually Defaults to None.
-            prompt_max_length (int, optional): max length of prompt. Defaults to 2800.
+            system_template (str, optional): System template, normally the role of LLM.
+            instruction_template (str, optional): Indicate the instruction for LLM.
+            user_template (str, optional): Prefix before user input. Defaults to ''.
+            exec_template (str, optional): A wrapper str for exec result.
+            assistant_template (str, optional): Prefix before assistant response.
+            Some LLM need to manully concat this prefix before generation.
+            prompt_max_length (int, optional): max length of prompt. Defaults to 2799.
+
         """
 
         self.system_template = system_template
@@ -160,8 +126,6 @@ class PromptGenerator:
         for i in range(len(self.history)):
             history_item = self.history[len(self.history) - i - 1]
             text = history_item['content']
-            # l = len(history_str) + len(text) + len(self.prompt)
-            # print(f'prompt length: {l}')
             if len(history_str) + len(text) + len(
                     self.prompt) > self.prompt_max_length:
                 break
