f7c2d16dd249388e3df7a82cdd820174f6dea7fe
nil.wyw <nil.wyw@alibaba-inc.com>
2023-11-15 17:07:03 +0800

    feat: Prompt Suggestions & encoding

--- demo/agents/config_utils.py
+++ demo/agents/config_utils.py
@@ -1,10 +1,10 @@
 import json
 import os
-from demo.agents.builder_core import DEFAULT_BUILDER_CONFIG_FILE
+from builder_core import DEFAULT_BUILDER_CONFIG_FILE
 
 
 def save_builder_configuration(builder_cfg):
     builder_cfg_file = os.getenv('BUILDER_CONFIG_FILE',
                                  DEFAULT_BUILDER_CONFIG_FILE)
-    with open(builder_cfg_file, 'w') as f:
-        f.write(json.dumps(builder_cfg, indent=2))
+    with open(builder_cfg_file, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(builder_cfg, indent=2, ensure_ascii=False))
