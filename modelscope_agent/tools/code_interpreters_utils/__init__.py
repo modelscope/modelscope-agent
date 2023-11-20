17cbc9e908b34c5bcb73192ccf9493cb83c4fe93
Zhicheng Zhang <zhangzhicheng.zzc@alibaba-inc.com>
2023-11-20 17:07:50 +0800

    remove open-interpreter dependency

---
+++ modelscope_agent/tools/code_interpreters_utils/__init__.py
@@ -0,0 +1,4 @@
+# all the utility functions under code_interpreters_utils are from project:
+# https://github.com/KillianLucas/open-interpreter
+
+from .base_code_interpreter import BaseCodeInterpreter
