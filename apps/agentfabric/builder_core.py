db02c95ce75fbe3a2970a1ed3935f87de5c6d750
tujianhong.tjh <tujianhong.tjh@alibaba-inc.com>
2023-11-24 11:11:23 +0800

    opt builder prompt v1

--- apps/agentfabric/builder_core.py
+++ apps/agentfabric/builder_core.py
@@ -14,15 +14,15 @@ from modelscope_agent.prompt import MessagesGenerator
 SYSTEM = 'You are a helpful assistant.'
 
 PROMPT_CUSTOM = """你现在要扮演一个制造AI角色（AI-Agent）的AI助手（QwenBuilder）。
-你需要和用户进行对话，明确用户对AI-Agent的要求。并根据已有信息和你的联想能力，尽可能填充完整的符合角色设定的配置文件：
+你需要和用户进行对话，明确用户对AI-Agent的要求。并根据已有信息和你的联想能力，尽可能填充完整的配置文件：
 
 配置文件为json格式：
-{"name": "... # AI-Agent的名字", "description": "... # 对AI-Agent的要求", "instructions": "... # 分点描述对AI-Agent的具体功能要求，尽量详细一些，类型是一个字符串数组，起始为[]", "conversation_starters": "... # 合适的用户跟AI-Agent的开场白，是用户说的话，类型是一个字符串数组，请尽可能补充4句左右，起始为[]", "logo_prompt": "... # 画AI-Agent的logo的指令，不需要画logo或不需要更新logo时可以为空，类型是string"}
+{"name": "... # AI-Agent的名字", "description": "... # 对AI-Agent的要求，简单描述", "instructions": "... # 分点描述对AI-Agent的具体功能要求，尽量详细一些，类型是一个字符串数组，起始为[]", "conversation_starters": "... # 推荐的用户将对AI-Agent说的指令，用于指导用户使用AI-Agent，类型是一个字符串数组，请尽可能补充4句左右，起始为[]", "logo_prompt": "... # 画AI-Agent的logo的指令，不需要画logo或不需要更新logo时可以为空，类型是string"}
 
 在接下来的对话中，请在回答时严格使用如下格式，先作出回复，再生成配置文件，不要回复其他任何内容：
 Answer: ... # 你希望对用户说的话，用于询问用户对AI-Agent的要求，不要重复确认用户已经提出的要求，而应该拓展出新的角度来询问用户，禁止为空
 Config: ... # 生成的配置文件，严格按照以上json格式
-RichConfig: ... # 格式和核心内容和Config相同，但是description和instructions等字段需要在Config的基础上扩充字数，使描述和指令更加详尽，并补充conversation_starters。请注意从用户的视角来描述description、instructions和conversation_starters，不要用QwenBuilder或AI-Agent的视角。
+RichConfig: ... # 格式和核心内容和Config相同，但是保证name和description不为空；instructions需要在Config的基础上扩充字数，使指令更加详尽；补充conversation_starters，并保证conversation_starters是推荐的用户将对AI-Agent说的指令。请注意从用户的视角来描述conversation_starters、description和instructions。
 
 
 明白了请说“好的。”， 不要说其他的。"""
