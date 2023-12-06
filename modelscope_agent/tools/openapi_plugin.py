424e72fbc75c6722ed963fc2944bc13dade1d3d1
wangyijunlyy <1157637345@qq.com>
2023-12-06 11:14:46 +0800

    fix

--- modelscope_agent/tools/openapi_plugin.py
+++ modelscope_agent/tools/openapi_plugin.py
@@ -283,10 +283,9 @@ def openapi_schema_convert(schema, auth):
             if security:
                 for sec in security:
                     if 'BearerAuth' in sec:
-                        api_token = auth.get('apikey', '')                          
-                        api_token_type = auth.get('apikey_type', 'Bearer'))
+                        api_token = auth.get('apikey', '')
+                        api_token_type = auth.get('apikey_type', 'Bearer')
                         authorization = f'{api_token_type} {api_token}'
-
             if method.upper() == 'POST':
                 requestBody = details.get('requestBody', {})
                 if requestBody:
