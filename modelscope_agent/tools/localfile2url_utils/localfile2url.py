b2ebd1019d0c125ebff1d0678f06333940cd4861
wangyijunlyy <1157637345@qq.com>
2023-12-19 15:44:49 +0800

    use dashscope to convert url

--- modelscope_agent/tools/localfile2url_utils/localfile2url.py
+++ modelscope_agent/tools/localfile2url_utils/localfile2url.py
@@ -1,48 +1,26 @@
 import os
-from configparser import ConfigParser
+from urllib.parse import unquote_plus, urlparse
 
-import oss2
+from dashscope.common.error import InvalidInput, UploadFileException
+from dashscope.utils.oss_utils import OssUtils
 
-WORK_DIR = os.getenv('CODE_INTERPRETER_WORK_DIR', '/tmp/ci_workspace')
-
-
-def upload_to_oss(bucket, local_file_path, oss_file_path):
-    # 上传文件到阿里云OSS
-    bucket.put_object_from_file(oss_file_path, local_file_path)
-
-    # 设置文件的公共读权限
-    bucket.put_object_acl(oss_file_path, oss2.OBJECT_ACL_PUBLIC_READ)
-
-    # 获取文件的公共链接
-    file_url = f"https://{bucket.bucket_name}.{bucket.endpoint.replace('http://', '')}/{oss_file_path}"
-    return file_url
+FILE_PATH_SCHEMA = 'file://'
 
 
-def get_oss_config():
-    # 尝试从环境变量中读取配置
-    access_key_id = os.getenv('OSS_ACCESS_KEY_ID')
-    access_key_secret = os.getenv('OSS_ACCESS_KEY_SECRET')
-    endpoint = os.getenv('OSS_ENDPOINT')
-    bucket_name = os.getenv('OSS_BUCKET_NAME')
-
-    # 如果环境变量没有设置，尝试从.ossutilconfig文件中读取
-    if not access_key_id or not access_key_secret or not endpoint or not bucket_name:
-        config = ConfigParser()
-        config.read(os.path.expanduser('~/.ossutilconfig'))
-        if 'Credentials' in config:
-            access_key_id = config.get('Credentials', 'accessKeyId')
-            access_key_secret = config.get('Credentials', 'accessKeySecret')
-            endpoint = config.get('Credentials', 'endpoint')
-            bucket_name = config.get('Credentials', 'bucketName')
-
-    return access_key_id, access_key_secret, endpoint, bucket_name
-
-
-def path2url(local_file_path, oss_file_path):
-    local_file_path = os.path.join(WORK_DIR, local_file_path)
-    ak_id, ak_secret, endpoint, bucket_name = get_oss_config()
-    auth = oss2.Auth(ak_id, ak_secret)
-    bucket = oss2.Bucket(auth, endpoint, bucket_name)
-    file_url = upload_to_oss(bucket, local_file_path,
-                             f'agents/user/{oss_file_path}')
-    return file_url
+def get_upload_url(model: str, upload_path: str, api_key: str):
+    if upload_path.startswith(FILE_PATH_SCHEMA):
+        parse_result = urlparse(upload_path)
+        if parse_result.netloc:
+            file_path = parse_result.netloc + unquote_plus(parse_result.path)
+        else:
+            file_path = unquote_plus(parse_result.path)
+        if os.path.exists(file_path):
+            file_url = OssUtils.upload(
+                model=model, file_path=file_path, api_key=api_key)
+            if file_url is None:
+                raise UploadFileException('Uploading file: %s failed'
+                                          % upload_path)
+            return file_url
+        else:
+            raise InvalidInput('The file: %s is not exists!' % file_path)
+    return None
