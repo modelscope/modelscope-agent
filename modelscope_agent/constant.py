5e91bbe2833fbbbd56d3044e97038d4bf169292c
suluyana <110878454+suluyana@users.noreply.github.com>
2023-12-26 10:39:22 +0800

    refine prompt generator mapping (#215)
    Co-authored-by: suluyan.sly <suluyan.sly@alibaba-inc.com>

--- modelscope_agent/constant.py
+++ modelscope_agent/constant.py
@@ -1,28 +1,34 @@
+from modelscope_agent.action_parser import (ChatGLMActionParser,
+                                            MRKLActionParser, MsActionParser,
+                                            OpenAiFunctionsActionParser)
+from modelscope_agent.prompt import (ChatGLMPromptGenerator, MessagesGenerator,
+                                     MrklPromptGenerator, MSPromptGenerator)
+
 DEFAULT_MODEL_CONFIG = {
     'qwen': {
         'en': {
-            'prompt_generator': 'MrklPromptGenerator',
-            'action_parser': 'MRKLActionParser'
+            'prompt_generator': MrklPromptGenerator,
+            'action_parser': MRKLActionParser
         },
         'zh': {
-            'prompt_generator': 'MrklPromptGenerator',
-            'action_parser': 'MRKLActionParser'
+            'prompt_generator': MrklPromptGenerator,
+            'action_parser': MRKLActionParser
         }
     },
     'qwen-plus': {
-        'prompt_generator': 'MessagesGenerator',
-        'action_parser': 'MRKLActionParser'
+        'prompt_generator': MessagesGenerator,
+        'action_parser': MRKLActionParser
     },
     'chatglm': {
-        'prompt_generator': 'ChatGLMPromptGenerator',
-        'action_parser': 'ChatGLMActionParser'
+        'prompt_generator': ChatGLMPromptGenerator,
+        'action_parser': ChatGLMActionParser
     },
     'gpt': {
-        'prompt_generator': 'MrklPromptGenerator',
-        'action_parser': 'MRKLActionParser'
+        'prompt_generator': MrklPromptGenerator,
+        'action_parser': MRKLActionParser
     },
     'openai': {
-        'prompt_generator': 'MrklPromptGenerator',
-        'action_parser': 'MRKLActionParser'
+        'prompt_generator': MrklPromptGenerator,
+        'action_parser': MRKLActionParser
     }
 }
