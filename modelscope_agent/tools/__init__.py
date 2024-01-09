316931bb219ee769cb058a7f812a257ed66c96d7
wangyijunlyy <1157637345@qq.com>
2024-01-09 16:08:03 +0800

    update

--- modelscope_agent/tools/__init__.py
+++ modelscope_agent/tools/__init__.py
@@ -2,9 +2,22 @@ from .amap_weather import AMAPWeather
 from .base import TOOL_REGISTRY, BaseTool
 from .code_interpreter_jupyter import CodeInterpreterJupyter
 from .dashscope_tools.image_generation import TextToImageTool
+from .dashscope_tools.qwen_vl import QWenVL
 from .dashscope_tools.style_repaint import StyleRepaint
+from .dashscope_tools.wordart_tool import WordArtTexture
 from .langchain_tool import LangchainTool
+from .modelscope_tools.image_chat_tool import ImageChatTool
+from .modelscope_tools.pipeline_tool import ModelscopePipelineTool
+from .modelscope_tools.text_address_tool import TextAddressTool
+from .modelscope_tools.text_ie_tool import TextInfoExtractTool
+from .modelscope_tools.text_ner_tool import TextNerTool
+from .modelscope_tools.text_to_speech_tool import TexttoSpeechTool
+from .modelscope_tools.text_to_video_tool import TextToVideoTool
+from .modelscope_tools.translation_en2zh_tool import TranslationEn2ZhTool
+from .modelscope_tools.translation_zh2en_tool import TranslationZh2EnTool
 from .openapi_plugin import OpenAPIPluginTool
+from .similarity_search import SimilaritySearch
+from .storage import Storage
 
 
 def call_tool(plugin_name: str, plugin_args: str) -> str:
