7be2f48508616fced421dc45b9578c552a712d6b
Zhicheng Zhang <zhangzhicheng.zzc@alibaba-inc.com>
2024-01-12 14:19:14 +0800

    update requirements

--- .dev_scripts/dockerci.sh
+++ .dev_scripts/dockerci.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
-MODELSCOPE_CACHE_DIR_IN_CONTAINER=/modelscope_cache
-IMAGE_NAME=registry.cn-hangzhou.aliyuncs.com/modelscope-repo/modelscope
-IMAGE_VERSION=ubuntu22.04-cuda11.8.0-py310-torch2.1.0-tf2.14.0-1.9.5-ci
-MODELSCOPE_DOMAIN=www.modelscope.cn
 
-CODE_DIR_IN_CONTAINER=/Maas-lib
-CI_COMMAND=pytest
-echo "$USER"
-echo "ci command: $CI_COMMAND"
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+pip config set install.trusted-host mirrors.aliyun.com
+git config --global --add safe.directory /Maas-lib
+git config --global user.email tmp
+git config --global user.name tmp.com
 
-CONTAINER_NAME="modelscope-agent-ci"
-# pull image if there are update
-docker pull ${IMAGE_NAME}:${IMAGE_VERSION}
-docker run --rm --name $CONTAINER_NAME --shm-size=16gb \
-            -e CI_TEST=True \
-            -e MODELSCOPE_CACHE=$MODELSCOPE_CACHE_DIR_IN_CONTAINER \
-            -e MODELSCOPE_DOMAIN=$MODELSCOPE_DOMAIN \
-            -e MODELSCOPE_ENVIRONMENT='ci' \
-            --workdir=$CODE_DIR_IN_CONTAINER \
-            ${IMAGE_NAME}:${IMAGE_VERSION} \
-            $CI_COMMAND
+pip install torch
 
-if [ $? -ne 0 ]; then
-  echo "Running test case failed, please check the log!"
-  exit -1
-fi
+pytest
