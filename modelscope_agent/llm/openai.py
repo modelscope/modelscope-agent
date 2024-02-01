782b034befa69a0c7082669f92d527cac3ad158d
suluyan <suluyan.sly@alibaba-inc.com>
2024-02-01 13:47:25 +0800

    update openai llm

--- modelscope_agent/llm/openai.py
+++ modelscope_agent/llm/openai.py
@@ -1,6 +1,7 @@
 import os
-from typing import Dict, Iterator, List, Optional
+from typing import Dict, Iterator, List, Optional, Union
 
+from modelscope_agent.utils.retry import retry
 from modelscope_agent.llm.base import BaseChatModel, register_llm
 from openai import OpenAI
 
@@ -8,24 +9,28 @@ from openai import OpenAI
 @register_llm('openai')
 class OpenAi(BaseChatModel):
 
-    def __init__(self, model: str, model_server: str, **kwargs):
+    def __init__(self, model: str, model_server: str, is_chat: bool = True, is_function_call: Optional[bool] = None, support_stream: Optional[bool] = None, **kwargs):
         super().__init__(model, model_server)
 
+        print(kwargs, model, model_server)
         api_base = kwargs.get('api_base', 'https://api.openai.com/v1').strip()
         api_key = kwargs.get('api_key',
                              os.getenv('OPENAI_API_KEY',
                                        default='EMPTY')).strip()
         self.client = OpenAI(api_key=api_key, base_url=api_base)
+        self.is_function_call = is_function_call
+        self.is_chat = is_chat
+        self.support_stream = support_stream
 
     def _chat_stream(self,
                      messages: List[Dict],
                      stop: Optional[List[str]] = None,
                      **kwargs) -> Iterator[str]:
-        response = self.client.completions.create(
+        response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
             stop=stop,
-            stream=True,
+            stream=True ,
             **kwargs)
         # TODO: error handling
         for chunk in response:
@@ -36,7 +41,7 @@ class OpenAi(BaseChatModel):
                         messages: List[Dict],
                         stop: Optional[List[str]] = None,
                         **kwargs) -> str:
-        response = self.client.completions.create(
+        response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
             stop=stop,
@@ -44,6 +49,56 @@ class OpenAi(BaseChatModel):
             **kwargs)
         # TODO: error handling
         return response.choices[0].message.content
+
+    def support_function_calling(self):
+        if self.is_function_call == None:
+            return super().support_function_calling()
+        else:
+            return self.is_function_call
+
+    def support_raw_prompt(self) -> bool:
+        if self.is_chat == None:
+            return super().support_raw_prompt()
+        else:
+            return self.is_chat
+
+    @retry(max_retries=3, delay_seconds=0.5)
+    def chat(self,
+             prompt: Optional[str] = None,
+             messages: Optional[List[Dict]] = None,
+             stop: Optional[List[str]] = None,
+             stream: bool = False,
+             **kwargs) -> Union[str, Iterator[str]]:
+        if isinstance(self.support_stream, bool):
+            stream = self.support_stream
+        if self.support_raw_prompt():
+            return self.chat_with_raw_prompt(prompt=prompt, stream=stream, stop=stop, **kwargs)
+        if not messages and prompt and isinstance(prompt, str):
+            messages = [{'role': 'user', 'content': prompt}]
+        return super().chat(messages=messages, stop=stop, stream=stream, **kwargs)
+
+    def chat_with_raw_prompt(self,
+                             prompt: str,
+                             stream: bool=True,
+                             **kwargs) -> str:
+        from prompts import prompts
+        prompt = prompts[self.model]
+        max_tokens = kwargs.get('max_tokens', 2000)
+        response = self.client.completions.create(
+            model=self.model,
+            prompt=prompt,
+            stream=stream,
+            max_tokens=max_tokens
+        )
+        print(response)
+        # TODO: error handling
+        if stream:
+            for chunk in response:
+                if hasattr(chunk.choices[0], 'text'):
+                    yield chunk.choices[0].text
+        else:
+            return response.choices[0].text
+
 
     def chat_with_functions(self,
                             messages: List[Dict],
@@ -61,38 +116,3 @@ class OpenAi(BaseChatModel):
         # TODO: error handling
         return response.choices[0].message
 
-
-@register_llm('openapi')
-class OpenAPILocal(BaseChatModel):
-
-    def __init__(self, model: str, model_server: str, **kwargs):
-        super().__init__(model, model_server)
-        openai_api_key = 'EMPTY'
-        openai_api_base = 'http://localhost:8000/v1'
-        self.client = OpenAI(
-            api_key=openai_api_key,
-            base_url=openai_api_base,
-        )
-
-    def _chat_stream(self, prompt: str, **kwargs) -> Iterator[str]:
-        response = self.client.completions.create(
-            model=self.model,
-            prompt=prompt,
-            stream=True,
-        )
-        # TODO: error handling
-        for chunk in response:
-            if hasattr(chunk.choices[0], 'text'):
-                yield chunk.choices[0].text
-
-    def _chat_no_stream(self, prompt: str, **kwargs) -> str:
-        response = self.client.completions.create(
-            model=self.model,
-            prompt=prompt,
-            stream=False,
-        )
-        # TODO: error handling
-        return response.choices[0].message.content
-
-    def support_function_calling(self) -> bool:
-        return False
