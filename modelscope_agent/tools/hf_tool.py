bbc546954ef07d2734af21b39a496b5a6d5bec7e
mushenL <mushenl0412@gmail.com>
2024-02-02 18:57:55 +0800

    add hf_tool

--- modelscope_agent/tools/hf_tool.py
+++ modelscope_agent/tools/hf_tool.py
@@ -1,19 +1,21 @@
+import json
 from modelscope_agent.tools.base import BaseTool, register_tool
 from transformers.tools import Tool as HFTool
-import json
+
 
 @register_tool('HFTool')
 class HFTool(BaseTool):
     description = '通过调用HFTool插件来支持文本，音频，图片等的生成'
     name = 'HFTool'
     parameters: list = []
+
     def __init__(self, HFTool):
         from transformers.tools import Tool as HF_Tool
         if not isinstance(HFTool, HF_Tool):
             raise ValueError('HFTool should be type of HF tool')
         self.HFTool = HFTool
         super().__init__()
-    
+
     def call(self, params: str, **kwargs):
         params = self._verify_args(params)
         res = self.HFTool(**params)
--- tests/tools/test_hf_tool.py
+++ tests/tools/test_hf_tool.py
@@ -1,6 +1,6 @@
 import pytest
-from transformers import load_tool
 from modelscope_agent.tools.hf_tool import HFTool
+from transformers import load_tool
 
 
 def test_is_hf_tool():
@@ -10,9 +10,10 @@ def test_is_hf_tool():
     exec_msg = e.value.args[0]
     assert (exec_msg == 'HFTool should be type of HF tool')
 
+
 def test_run_langchin_tool():
     # test run hf tool
-    tool = load_tool("translation")
+    tool = load_tool('translation')
     shell_tool = HFTool(tool)
     input = """{'text': 'Hello','src_lang':'English','tgt_lang':'French'}"""
     res = shell_tool.call(input)
