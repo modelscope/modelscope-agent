858170cacb0c0bd055cbabb3192942d257cb6d65
skyline2006 <skyline2006@163.com>
2024-02-05 15:30:21 +0800

    update ci_env

--- .dev_scripts/dockerci.sh
+++ .dev_scripts/dockerci.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
 # install dependencies for ci
+apt install ffmpeg
 pip install torch
 export CODE_INTERPRETER_WORK_DIR=${GITHUB_WORKSPACE}
 echo "${CODE_INTERPRETER_WORK_DIR}"
 
 # cp file
-cp tests/samples/luoli15.jpg "${CODE_INTERPRETER_WORK_DIR}/luoli15.jpg"
+cp tests/samples/* "${CODE_INTERPRETER_WORK_DIR}/"
 ls  "${CODE_INTERPRETER_WORK_DIR}"
 
 # run ci
