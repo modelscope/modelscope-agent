79d54857bcf5694a779bdbb6dd71c4e9b8e180ad
shenwzh3 <shenwzh3@mail2.sysu.edu.cn>
2024-02-29 17:08:52 +0800

    add rapidapi tool test

--- tests/tools/test_rapid_api_tool.py
+++ tests/tools/test_rapid_api_tool.py
@@ -1,7 +1,6 @@
 def test_rapidapi_current_exchange():
-    from modelscope_agent.tools.rapidapi_tools.Finance.current_exchage import \
-    (listquotes_for_current_exchange,
-     exchange_for_current_exchange)
+    from modelscope_agent.tools.rapidapi_tools.Finance.current_exchage import (
+        listquotes_for_current_exchange, exchange_for_current_exchange)
     kwargs = """{}"""
     tool = listquotes_for_current_exchange()
     res = tool.call(kwargs)
@@ -13,10 +12,9 @@ def test_rapidapi_current_exchange():
 
 
 def test_rapidapi_numbers():
-    from modelscope_agent.tools.rapidapi_tools.Number.numbers import \
-    (get_data_fact_for_numbers,
-     get_math_fact_for_numbers,
-     get_year_fact_for_numbers)
+    from modelscope_agent.tools.rapidapi_tools.Number.numbers import (
+        get_data_fact_for_numbers, get_math_fact_for_numbers,
+        get_year_fact_for_numbers)
     kwargs = """{'month':'7', 'day':'1'}"""
     tool = get_data_fact_for_numbers()
     res = tool.call(kwargs)
@@ -32,30 +30,34 @@ def test_rapidapi_numbers():
 
 
 def test_rapidapi_movie_tv_music_search_and_download():
-    from modelscope_agent.tools.rapidapi_tools.Movies.movie_tv_music_search_and_download import \
-    (search_torrents_for_movie_tv_music_search_and_download,
-     get_monthly_top_100_games_torrents_for_movie_tv_music_search_and_download,
-     get_monthly_top_100_movies_torrents_torrents_for_movie_tv_music_search_and_download,
-     get_monthly_top_100_music_torrents_for_movie_tv_music_search_and_download,
-     get_monthly_top_100_tv_shows_torrents_for_movie_tv_music_search_and_download)
+    from modelscope_agent.tools.rapidapi_tools.Movies.movie_tv_music_search_and_download import (
+        search_torrents_for_movie_tv_music_search_and_download,
+        get_monthly_top_100_games_torrents_for_movie_tv_music_search_and_download,
+        get_monthly_top_100_movies_torrents_torrents_for_movie_tv_music_search_and_download,
+        get_monthly_top_100_music_torrents_for_movie_tv_music_search_and_download,
+        get_monthly_top_100_tv_shows_torrents_for_movie_tv_music_search_and_download
+    )
     kwargs = """{'keywords':'sports', 'quantity':'10'}"""
     tool = search_torrents_for_movie_tv_music_search_and_download()
     res = tool.call(kwargs)
     assert isinstance(res, dict) or isinstance(res, str)
     kwargs = """{}"""
-    tool = get_monthly_top_100_games_torrents_for_movie_tv_music_search_and_download()
+    tool = get_monthly_top_100_games_torrents_for_movie_tv_music_search_and_download(
+    )
     res = tool.call(kwargs)
     assert isinstance(res, dict) or isinstance(res, str)
     kwargs = """{}"""
-    tool = get_monthly_top_100_movies_torrents_torrents_for_movie_tv_music_search_and_download()
+    tool = get_monthly_top_100_movies_torrents_torrents_for_movie_tv_music_search_and_download(
+    )
     res = tool.call(kwargs)
     assert isinstance(res, dict) or isinstance(res, str)
     kwargs = """{}"""
-    tool = get_monthly_top_100_music_torrents_for_movie_tv_music_search_and_download()
+    tool = get_monthly_top_100_music_torrents_for_movie_tv_music_search_and_download(
+    )
     res = tool.call(kwargs)
     assert isinstance(res, dict) or isinstance(res, str)
     kwargs = """{}"""
-    tool = get_monthly_top_100_tv_shows_torrents_for_movie_tv_music_search_and_download()
+    tool = get_monthly_top_100_tv_shows_torrents_for_movie_tv_music_search_and_download(
+    )
     res = tool.call(kwargs)
     assert isinstance(res, dict) or isinstance(res, str)
-
