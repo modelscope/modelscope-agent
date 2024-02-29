36ad9261cf3e3defb92e6524b81ebf4a45f49035
Zhicheng Zhang <zczhang.hvac@gmail.com>
2024-02-29 18:13:20 +0800

    update sh

--- demo/alpha_umi/run_test.sh
+++ demo/alpha_umi/run_test.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=./
-export RAPID_API_TOKEN="1010919056msh03dc548f7c5ff35p1e0988jsndc80beff3fa8"
-export MODELSCOPE_API_TOKEN="b9144b02-b94c-4c62-956d-a4c8228854e9"
+export RAPID_API_TOKEN="your rapid api token here"
+export MODELSCOPE_API_TOKEN="your modelscope api token here"
 
 python demo/alpha_umi/test_alpha_umi.py
