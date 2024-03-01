435c3daaed2cb74b20444dd912e83d3e231e2556
Zhicheng Zhang <zczhang.hvac@gmail.com>
2024-03-01 10:00:15 +0800

    update requirements

--- modelscope_agent/__init__.py
+++ modelscope_agent/__init__.py
@@ -1,20 +1,37 @@
 import ray
-from ray.util.client.common import ClientActorHandle, ClientObjectRef
 
 from .agent import Agent
 
 
-def _create_remote(cls,
-                   name,
-                   max_concurrency=1,
-                   *args,
-                   **kwargs) -> ClientActorHandle:
+def _create_remote(cls, name, max_concurrency=1, *args, **kwargs):
+    '''
+    Create a remote actor by ray
+    Args:
+        cls: the class to be created
+        name: the name of ray actor
+        max_concurrency: max concurrency of the actor
+        *args:
+        **kwargs:
+
+    Returns:
+
+    '''
     return ray.remote(
         name=name,
         max_concurrency=max_concurrency)(cls).remote(*args, **kwargs)
 
 
 def _create_local(cls, *args, **kwargs):
+    '''
+    Create a local object
+    Args:
+        cls: the class to be created
+        *args:
+        **kwargs:
+
+    Returns:
+
+    '''
     return cls(*args, **kwargs)
 
 
