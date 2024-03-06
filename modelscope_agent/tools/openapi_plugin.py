6090e5638a183972e6d90c93d55f1ed07330f92f
yangkun <yk01645326@alibaba-inc.com>
2024-03-06 14:57:57 +0800

    openapi_plugin change default values of env

--- modelscope_agent/tools/openapi_plugin.py
+++ modelscope_agent/tools/openapi_plugin.py
@@ -278,9 +278,9 @@ def openapi_schema_convert(schema, auth):
             if security:
                 for sec in security:
                     if 'BearerAuth' in sec:
-                        api_token = auth.get('apikey', os.environ['apikey'])
+                        api_token = auth.get('apikey', os.environ.get('apikey', ''))
                         api_token_type = auth.get('apikey_type',
-                                                  os.environ['apikey_type'])
+                                                  os.environ.get('apikey_type', 'Bearer'))
                         authorization = f'{api_token_type} {api_token}'
             if method.upper() == 'POST':
                 requestBody = details.get('requestBody', {})
