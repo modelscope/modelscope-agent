7b874bc469899e0e3910211d3f064bbf2a2a6922
Zhicheng Zhang <zczhang.hvac@gmail.com>
2024-03-11 14:32:27 +0800

    update gradio case

--- apps/multi_roles_chat_room/app.py
+++ apps/multi_roles_chat_room/app.py
@@ -3,45 +3,19 @@ import re
 import gradio as gr
 import json
 import modelscope_studio as mgr
-from role_core import (chat_progress, init_all_remote_actors, roles,
-                       start_chat_with_topic)
+from role_core import chat_progress, init_all_remote_actors
+from role_core import roles as origin_roles
+from role_core import start_chat_with_topic
 
 chat_history = []
 
-
 # 发送消息的函数
-def send_message(_chatbot, _input, _state):
-    _chatbot.append([_input.text, None])
-    yield {
-        state: _state,
-        preview_chat_input: gr.update(interactive=False, value=None),
-        user_chatbot: mgr.Chatbot(visible=True, value=_chatbot)
-    }
-
-    bot_messages = {key: '' for key in _state['role_names']}
-
-    for frame_text in chat_progress(_chatbot.text, _state):
-        role, content = get_frame_data(frame_text)
-        if role in bot_messages:
-            bot_messages[role] += content
-        _chatbot[-1][1] = [bot_messages[key] for key in _state['role_names']]
-
-        # try to parse the next_speakers from yield result
-        try:
-            next_speakers = json.loads(frame_text)['next_speakers']
-            _state['next_agent_names'] = next_speakers
-            yield {state: _state}
-        except Exception:
-            pass
-
-    yield {
-        user_chatbot: _chatbot,
-    }
 
 
 def render_json_as_markdown(json_data):
     json_str = json.dumps(json_data, indent=2, ensure_ascii=False)
     markdown_str = '角色信息\n```json\n' + json_str + '\n```'
+    print(markdown_str)
     return markdown_str
 
 
@@ -60,8 +34,8 @@ def get_frame_data(text):
 
 
 # update or add user
-def upsert_user(new_user, user_char):
-
+def upsert_user(new_user, user_char, _state):
+    roles = _state['roles']
     if new_user and new_user not in roles:
         roles[new_user] = user_char
         return gr.update(
@@ -75,36 +49,6 @@ def upsert_user(new_user, user_char):
 
 
 # start topic
-def start_chat(username, from_user, topic, _state, _chatbot, _input):
-    _state = init_all_remote_actors(roles, username, _state)
-    _state = start_chat_with_topic(from_user, topic, _state)
-
-    _chatbot.append([topic, None])
-    yield {
-        state: _state,
-        preview_chat_input: gr.update(interactive=False, value=None),
-        user_chatbot: _chatbot
-    }
-
-    bot_messages = {key: '' for key in _state['role_names']}
-
-    for frame_text in chat_progress(None, _state):
-        role, content = get_frame_data(frame_text)
-        if role in bot_messages:
-            bot_messages[role] += content
-        _chatbot[-1][1] = [bot_messages[key] for key in _state['role_names']]
-
-        # try to parse the next_speakers from yield result
-        try:
-            next_speakers = json.loads(frame_text)['next_speakers']
-            _state['next_agent_names'] = next_speakers
-            yield {state: _state}
-        except Exception:
-            pass
-
-    yield {
-        user_chatbot: _chatbot,
-    }
 
 
 # end topic
@@ -115,7 +59,7 @@ def end_topic():
 
 # 创建Gradio界面
 with gr.Blocks() as demo:
-    state = gr.State({})
+    state = gr.State({'roles': origin_roles})
     with gr.Row():
         with gr.Column(scale=2):
             user_chatbot = mgr.Chatbot(
@@ -124,11 +68,11 @@ with gr.Blocks() as demo:
                 elem_classes=['markdown-body'],
                 avatar_images=[None],
                 height=650,
-                show_label=False,
-                visible=False,
+                show_label=True,
+                visible=True,
                 show_copy_button=True)
             preview_chat_input = mgr.MultimodalInput(
-                interactive=False,
+                interactive=True,
                 label='输入',
                 placeholder='输入你的消息',
                 submit_button_props=dict(label='发送（role 加载中...）'))
@@ -141,25 +85,86 @@ with gr.Blocks() as demo:
                     placeholder='input role characters ...')
                 new_user_btn = gr.Button('Add/Update role infos')
                 role_info = gr.Textbox(label='Result', interactive=False)
-                all_roles = gr.Markdown(render_json_as_markdown(roles))
+                all_roles = mgr.Markdown(render_json_as_markdown(origin_roles))
 
             with gr.Group('Chat Room'):
                 start_topic_from = gr.Dropdown(
                     label='The role who start the topic',
-                    choices=list(roles.keys()),
-                    value=list(roles.keys())[1])
+                    choices=list(origin_roles.keys()),
+                    value=list(origin_roles.keys())[1])
                 start_topic_input = gr.Textbox(
                     label='Topic to be discussed',
                     placeholder='@顾易 要不要来我家吃饭？',
                     value='@顾易 要不要来我家吃饭？')
                 user_select = gr.Dropdown(
                     label='Role playing',
-                    choices=list(roles.keys()),
-                    value=list(roles.keys())[0])
+                    choices=list(origin_roles.keys()),
+                    value=list(origin_roles.keys())[0])
                 start_chat_btn = gr.Button('Start new chat')
                 # end_chat_btn = gr.Button("End chat")
 
-    input = mgr.MultimodalInput()
+    def start_chat(username, from_user, topic, _state, _chatbot, _input):
+        roles = _state['roles']
+        _state = init_all_remote_actors(roles, username, _state)
+        _state = start_chat_with_topic(from_user, topic, _state)
+
+        _chatbot.append([topic, None])
+        yield {
+            state: _state,
+            preview_chat_input: gr.update(interactive=False, value=None),
+            user_chatbot: _chatbot
+        }
+
+        bot_messages = {key: '' for key in _state['role_names']}
+
+        for frame_text in chat_progress(None, _state):
+            role, content = get_frame_data(frame_text)
+            if role in bot_messages:
+                bot_messages[role] += content
+            _chatbot[-1][1] = [
+                bot_messages[key] for key in _state['role_names']
+            ]
+            yield {
+                user_chatbot: _chatbot,
+            }
+
+            # try to parse the next_speakers from yield result
+            try:
+                next_speakers = json.loads(frame_text)['next_agent_names']
+                _state['next_agent_names'] = next_speakers
+                yield {state: _state}
+            except Exception:
+                pass
+
+    def send_message(_chatbot, _input, _state):
+        _chatbot.append([_input.text, None])
+        yield {
+            state: _state,
+            preview_chat_input: gr.update(interactive=False, value=None),
+            user_chatbot: mgr.Chatbot(visible=True, value=_chatbot)
+        }
+
+        bot_messages = {key: '' for key in _state['role_names']}
+
+        for frame_text in chat_progress(_chatbot.text, _state):
+            role, content = get_frame_data(frame_text)
+            if role in bot_messages:
+                bot_messages[role] += content
+            _chatbot[-1][1] = [
+                bot_messages[key] for key in _state['role_names']
+            ]
+
+            # try to parse the next_speakers from yield result
+            try:
+                next_agent_names = json.loads(frame_text)['next_agent_names']
+                _state['next_agent_names'] = next_agent_names
+                yield {state: _state}
+            except Exception:
+                pass
+
+        yield {
+            user_chatbot: _chatbot,
+        }
 
     # send message btn
     preview_chat_input.submit(
@@ -170,7 +175,7 @@ with gr.Blocks() as demo:
     # add or update role btn
     new_user_btn.click(
         fn=upsert_user,
-        inputs=[new_user_name, new_user_char],
+        inputs=[new_user_name, new_user_char, state],
         outputs=[user_select, role_info, all_roles])
 
     # start new chat btn
