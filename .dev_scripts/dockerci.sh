b368b7ce261b2a197a954216bffa98a7dfc4cfe2
Zhicheng Zhang <zczhang.hvac@gmail.com>
2024-03-15 20:26:08 +0800

    ffmpeg download stuck, temparily jump

--- .dev_scripts/dockerci.sh
+++ .dev_scripts/dockerci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install dependencies for ci
-wget -O ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar xvf ffmpeg.tar.xz
+#wget -O ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+#tar xvf ffmpeg.tar.xz
 export export PATH=`pwd`:$PATH
 pip install torch
 export CODE_INTERPRETER_WORK_DIR=${GITHUB_WORKSPACE}
