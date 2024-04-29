1ed2e252e65692b453d7d89bc0bac4afac66f276
杨堃 <yangkun@beyondosftdeMacBook-Pro.local>
2024-04-29 18:13:47 +0800

    Add log for bug about openai api config save fail,fix #425

--- apps/agentfabric/server.py
+++ apps/agentfabric/server.py
@@ -278,8 +278,8 @@ def get_builder_file(uuid_str, file_name):
 @with_request_id
 def save_builder_config(uuid_str):
     logger.info(f'save_builder_config: uuid_str_{uuid_str}')
-
     builder_config_str = request.form.get('builder_config')
+    logger.info(f'builder_config: {builder_config_str}')
     builder_config = json.loads(builder_config_str)
     if 'knowledge' in builder_config:
         builder_config['knowledge'] = [
