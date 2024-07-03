f2d878f243a6ad1854265934cb3db328b7cf30ee
suluyana <110878454+suluyana@users.noreply.github.com>
2024-07-03 18:45:12 +0800

    hotfix: rm useless test file & add vector_stores for multiple stores (#512)

--- tests/samples/code.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-from llama_index.core import VectorStoreIndex
-from llama_index.core.base.base_retriever import BaseRetriever
-from llama_index.core.schema import NodeWithScore, QueryBundle
-from llama_index.retrievers.bm25 import BM25Retriever
