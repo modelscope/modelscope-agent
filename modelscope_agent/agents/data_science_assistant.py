0373d074a1482277275f022b8afc30ae4e624e19
Dahai Peng <chenjieshupdh@foxmail.com>
2024-07-05 17:29:24 +0800

    adding update task logic

--- modelscope_agent/agents/data_science_assistant.py
+++ modelscope_agent/agents/data_science_assistant.py
@@ -43,25 +43,39 @@ Output a list of jsons following the format:
 ]
 ```
 """
-CODE_TEMPLATE = """you are a code interpreter, you need to generate a new jupyter notebook code block based on the \
-instruction: {instruction}, the code format is as follows:
+CODE_TEMPLATE = """
+you are a code fixer, you need to fix python a code block in jupyter notebook to achieve the\
+current task:
+{instruction}
+
+current task is part of the whole plan to achieve the user request:
+{user_request}
+
+the code format is as follows:
 ```python
 # the code you need to write
 ```
 previous code are as follows, you need to generate python code that follows previous code, no need to repeat previous \
 code:
-{previous_code} 
+{previous_code}
 Attention: the code format MUST be followed, otherwise the code interpreter will not be able to parse\
-the code correctly,the code format is as follows: 
-```python 
-# the code you need to write 
+the code correctly,the code format is as follows:
+```python
+# the code you need to write
 ```
 """
-CODE_REFLECT_TEMPLATE = """you are a code fixer, you need to fix python a code block in jupyter notebook to achieve the \
-goal: {instruction},
-the code format is as follows: 
-```python 
-# the code you need to write 
+CODE_REFLECT_TEMPLATE = """
+
+you are a code fixer, you need to fix python a code block in jupyter notebook to achieve the\
+current task:
+{instruction}
+
+current task is part of the whole plan to achieve the user request:
+{user_request}
+
+the code format is as follows:
+```python
+# the code you need to write
 ```
 
 the code you need to fix is as follows:
@@ -76,15 +90,16 @@ please correct the code and try again
 previous code are as follows and have been executed successfully in the previous jupyter notebook code blocks, \
 which means you can use the variables defined in the previous code blocks.
 the code you need to fix should follow previous code, no need to repeat
-previous code: 
+previous code:
 {previous_code}
 
 Attention: the code format MUST be followed, otherwise the code interpreter will not be able to parse the code \
-correctly,the code format is as follows: 
-```python 
-# the code you need to write 
+correctly,the code format is as follows:
+```python
+# the code you need to write
 ```
 """
+ERROR_KEYWORDS = ['Error', 'error', 'ERROR', 'Traceback', 'exception']
 
 
 class DataScienceAssistant(RolePlay):
@@ -97,6 +112,7 @@ class DataScienceAssistant(RolePlay):
                  description: Optional[str] = None,
                  instruction: Union[str, dict] = None,
                  **kwargs):
+
         super().__init__(
             function_list=function_list,
             llm=llm,
@@ -105,6 +121,7 @@ class DataScienceAssistant(RolePlay):
             description=description,
             instruction=instruction,
             **kwargs)
+        self.code_interpreter = CodeInterpreter()
 
     def _update_plan(self, user_request: str, curr_plan: Plan = None) -> Plan:
         resp = self._call_llm(
@@ -126,20 +143,18 @@ class DataScienceAssistant(RolePlay):
         else:
             if len(tasks) == 1 or tasks[0].dependent_task_ids:
                 if tasks[0].dependent_task_ids and len(tasks) > 1:
-                    # tasks[0].dependent_task_ids means the generated tasks are not a complete plan
-                    # for they depend on tasks in the current plan, in this case, we only support updating one task each time
                     logger.warning(
-                        "Current plan will take only the first generated task if the generated tasks are not a complete plan"
-                    )
-                # handle a single task
+                        'Current plan will take only the first generated task if the generated tasks are not a '
+                        'complete plan')
+
                 if curr_plan.has_task_id(tasks[0].task_id):
-                    # replace an existing task
+
                     curr_plan.replace_task(tasks[0])
                 else:
-                    # append one task
+
                     curr_plan.append_task(tasks[0])
             else:
-                # add tasks in general
+
                 curr_plan.add_tasks(tasks)
             return curr_plan
 
@@ -152,23 +167,16 @@ class DataScienceAssistant(RolePlay):
         with open(file_name, 'w', encoding='utf-8') as file:
             nbformat.write(nb, file)
 
-    def _run(self,
-             user_request,
-             history: Optional[List[Dict]] = None,
-             ref_doc: str = None,
-             image_url: Optional[List[Union[str, Dict]]] = None,
-             lang: str = 'zh',
-             save: bool = True,
-             **kwargs):
+    def _run(self, user_request, save: bool = True, **kwargs):
         try:
             plan = self._update_plan(user_request=user_request)
             logger.info(f'plan: {plan}')
-            code_interpreter = CodeInterpreter()
+
             while plan.current_task_id:
                 task = plan.task_map.get(plan.current_task_id)
                 logger.info(f'task: {task}')
                 previous_code = ''
-                for cell in code_interpreter.nb.cells:
+                for cell in self.code_interpreter.nb.cells:
                     error = False
                     if cell.cell_type == 'code':
                         for out_put in cell.outputs:
@@ -176,52 +184,55 @@ class DataScienceAssistant(RolePlay):
                                 error = True
                         if not error:
                             previous_code += cell.source
-
                 success = False
                 counter = 0
                 resp = ''
                 code = ''
-                print(f'previous_code:\n {previous_code}')
-                while not success and counter < 6:
-                    print(f'task_id: {task.task_id}, counter: {counter}')
-                    print("code:\n", code)
+
+                while not success and counter < 5:
+
                     if counter == 0:
+                        # first time to generate code
                         prompt = CODE_TEMPLATE.format(
                             instruction=task.instruction,
-                            previous_code=previous_code)
+                            previous_code=previous_code,
+                            user_request=user_request)
                     else:
+                        # reflect the error and ask user to fix the code
                         prompt = CODE_REFLECT_TEMPLATE.format(
                             instruction=task.instruction,
                             previous_code=previous_code,
                             code=code,
+                            user_request=user_request,
                             error=resp[:10000])
                     resp = self._call_llm(
                         prompt=prompt,
                         messages=None,
                         stop=None,
-                        )
+                    )
 
                     code = ''
                     for chunk in resp:
                         code += chunk
                     code = parse_code(text=code, lang='python')
                     try:
-                        resp = code_interpreter.call(params=json.dumps({'code': code}), nb_mode=True)
-                        print("code interpreter response:\n", resp)
-                        if 'Error' in resp or 'error' in resp or 'ERROR' in resp or 'Traceback' in resp or 'exception' in resp:
-                            success = False
-                        else:
-                            success = True
+                        # call code interpreter to execute the code
+                        resp = self.code_interpreter.call(
+                            params=json.dumps({'code': code}), nb_mode=True)
+                        success = not any(keyword in resp
+                                          for keyword in ERROR_KEYWORDS)
                     except Exception as e:
-                        print(f'error: {e}')
+                        success = False
+                        resp = 'Error: ' + str(e)
 
                     counter += 1
                 if success:
                     plan.finish_current_task()
                 else:
-                    plan = self._update_plan(user_request=user_request, curr_plan=plan)
+                    plan = self._update_plan(
+                        user_request=user_request, curr_plan=plan)
             if save:
-                self._save(code_interpreter.nb)
+                self._save(self.code_interpreter.nb)
         except Exception as e:
             logger.error(f'error: {e}')
             raise e
