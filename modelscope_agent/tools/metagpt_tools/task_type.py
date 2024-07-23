cb053933d514c821eb23a1f99bb8c32157207f68
Dahai Peng <chenjieshupdh@foxmail.com>
2024-07-23 20:37:02 +0800

    delete yml

--- modelscope_agent/tools/metagpt_tools/task_type.py
+++ modelscope_agent/tools/metagpt_tools/task_type.py
@@ -21,9 +21,6 @@ The current task is about data preprocessing, please note the following:
 - Ensure operations are on existing dataset columns.
 - Avoid writing processed data to files.
 - Avoid any change to label column, such as standardization, etc.
-- Prefer alternatives to one-hot encoding for categorical data.
-- Only encode or scale necessary columns to allow for potential feature-specific engineering tasks (like time_extract,\
- binning, extraction, etc.) later.
 - Each step do data preprocessing to train, must do same for test separately at the same time.
 - Always copy the DataFrame before processing it and use the copy to process.
 """
@@ -31,9 +28,7 @@ The current task is about data preprocessing, please note the following:
 # Prompt for taking on "feature_engineering" tasks
 FEATURE_ENGINEERING_PROMPT = """
 The current task is about feature engineering. when performing it, please adhere to the following principles:
-- Use available feature engineering tools if they are potential impactful.
 - Avoid creating redundant or excessively numerous features in one step.
-- Exclude ID columns from feature generation and remove them.
 - Each feature engineering operation performed on the train set must also applies to the \
 test separately at the same time.
 - Avoid using the label column to create features, except for cat encoding.
@@ -55,15 +50,6 @@ The current task is about evaluating a model, please note the following:
 - Ensure that the evaluated data is same processed as the training data. If not, remember \
 use object in 'Done Tasks' to transform the data.
 - Use trained model from previous task result directly, do not mock or reload model yourself.
-"""
-
-# Prompt for taking on "image2webpage" tasks
-IMAGE2WEBPAGE_PROMPT = """
-The current task is about converting image into webpage code. please note the following:
-- Single-Step Code Generation: Execute the entire code generation process in a single step, encompassing HTML, \
-CSS, and JavaScript. Avoid fragmenting the code generation into multiple separate steps to maintain consistency \
-and simplify the development workflow.
-- Save webpages: Be sure to use the save method provided.
 """
 
 
@@ -103,28 +89,9 @@ class TaskType(Enum):
         desc='Only for evaluating model.',
         guidance=MODEL_EVALUATE_PROMPT,
     )
-    IMAGE2WEBPAGE = TaskTypeDef(
-        name='image2webpage',
-        desc='For converting image into webpage code.',
-        guidance=IMAGE2WEBPAGE_PROMPT,
-    )
+
     OTHER = TaskTypeDef(
         name='other', desc='Any tasks not in the defined categories')
-
-    # Legacy TaskType to support tool recommendation using type match.
-    # You don't need to define task types if you have no human priors to inject.
-    TEXT2IMAGE = TaskTypeDef(
-        name='text2image',
-        desc='Related to text2image, image2image using stable diffusion model.',
-    )
-    WEBSCRAPING = TaskTypeDef(
-        name='web scraping',
-        desc='For scraping data from web pages.',
-    )
-    EMAIL_LOGIN = TaskTypeDef(
-        name='email login',
-        desc='For logging to an email.',
-    )
 
     @property
     def type_name(self):
