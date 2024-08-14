8f0cee9af6bbe642110cdb72e7e8236d268be0b5
Dahai Peng <chenjieshupdh@foxmail.com>
2024-08-14 13:17:49 +0800

    add upload file feature

--- apps/datascience_assistant/app.py
+++ apps/datascience_assistant/app.py
@@ -1,10 +1,14 @@
 import os
+import sys
 
 import streamlit as st
 from modelscope_agent.agents.data_science_assistant import DataScienceAssistant
 from modelscope_agent.tools.metagpt_tools.tool_recommend import \
     TypeMatchToolRecommender
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root_path = os.path.abspath(os.path.join(current_dir, '../../'))
+sys.path.append(project_root_path)
 llm_config = {
     'model': 'qwen2-72b-instruct',
     'model_server': 'dashscope',
@@ -17,7 +21,15 @@ st.title('Data Science Assistant')
 st.write(
     'This is a data science assistant that can help you with your data science tasks.'
 )
-st.write('Please input your request below and click the submit button.')
-user_request = st.text_input('User Request')
+st.write(
+    'Please input your request and upload files then click the submit button.')
+
+files = st.file_uploader(
+    'Please upload files that you need. ', accept_multiple_files=True)
+last_file_name = ''
+user_request = st.text_area('User Request')
 if st.button('submit'):
+    for file in files:
+        with open(file.name, 'wb') as f:
+            f.write(file.getbuffer())
     data_science_assistant.run(user_request=user_request, streamlit=True)
