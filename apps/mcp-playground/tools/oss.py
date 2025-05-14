1e65f30b71931e51497978be31cdb928dd3c9400
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-05-14 11:21:26 +0800

    update publish.yaml: ubuntu-22.04

--- apps/mcp-playground/tools/oss.py
+++ apps/mcp-playground/tools/oss.py
@@ -1,8 +1,10 @@
-from http import HTTPStatus
 import uuid
+from http import HTTPStatus
+
 import oss2
+from env import bucket_name, endpoint, region
 from oss2.credentials import EnvironmentVariableCredentialsProvider
-from env import endpoint, bucket_name, region
+
 # OSS_ACCESS_KEY_ID and OSS_ACCESS_KEY_SECRET。
 auth = oss2.ProviderAuthV4(EnvironmentVariableCredentialsProvider())
 
@@ -10,15 +12,13 @@ bucket = oss2.Bucket(auth, endpoint, bucket_name, region=region)
 
 
 def file_path_to_oss_url(file_path: str):
-    if file_path.startswith("http"):
+    if file_path.startswith('http'):
         return file_path
     ext = file_path.split('.')[-1]
     object_name = f'studio-temp/mcp-playground/{uuid.uuid4()}.{ext}'
     response = bucket.put_object_from_file(object_name, file_path)
     file_url = file_path
     if response.status == HTTPStatus.OK:
-        file_url = bucket.sign_url('GET',
-                                   object_name,
-                                   60 * 60,
-                                   slash_safe=True)
+        file_url = bucket.sign_url(
+            'GET', object_name, 60 * 60, slash_safe=True)
     return file_url
